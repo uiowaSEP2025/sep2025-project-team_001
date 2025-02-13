08a25c2d228a4ea1e1adeec31dc7422bbeb71ce7
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2025-02-13 12:10:19 -0600

    SCRUM-13 implemented keyboard logic

--- mobile_app/lib/authentication/authentication_page.dart
+++ mobile_app/lib/authentication/authentication_page.dart
@@ -18,163 +18,174 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
     double spacing = screenWidth * 0.05;
 
     return Scaffold(
-        body: Container(
-      width: screenWidth,
-      height: screenHeight,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xff1A1D1A), Color(0xff313538)], // Gradient colors
-          begin: Alignment.topLeft, // Gradient start position
-          end: Alignment.bottomRight, // Gradient end position
-        ),
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        SizedBox(
-          height: screenWidth * 0.2,
-        ),
-        Container(
-          width: screenWidth * 0.7,
-          child: Image.asset("assets/images/StreamlineLogo.png"),
-        ),
-        SizedBox(
-          height: spacing / 2,
-        ),
-        Container(
-          width: screenWidth * 0.75,
-          child: Text(
-            "Please sign in to start enjoying special treatment from your favorite restaurant",
-            textAlign: TextAlign.center,
-            style: AppTextStyles.subtitleParagraph(
-                screenHeight, AppColors.whiteText),
+        body: GestureDetector(
+      onTap: () {
+    FocusManager.instance.primaryFocus?.unfocus();
+  },
+      child: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff1A1D1A), Color(0xff313538)], // Gradient colors
+            begin: Alignment.topLeft, // Gradient start position
+            end: Alignment.bottomRight, // Gradient end position
           ),
         ),
-        SizedBox(
-          height: spacing,
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: spacing, right: spacing),
-          child: InputTextBox(
-            label: "Email",
-            hintText: "johndoe@gmail.com",
-            screenWidth: screenWidth,
-            screenHeight: screenHeight,
-          ),
-        ),
-        SizedBox(
-          height: spacing,
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: spacing, right: spacing),
-          child: InputTextBox(
-            label: "Password",
-            hintText: "********",
-            screenWidth: screenWidth,
-            screenHeight: screenHeight,
-          ),
-        ),
-        SizedBox(
-          height: spacing * 2,
-        ),
-        ElevatedButton(
-            onPressed: signIn(),
-            child: Container(
-              height: screenWidth * 0.12,
-              width: screenWidth - spacing * 2,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: AppColors.primaryColor),
-              child: Center(
-                child: Text(
-                  "SIGN UP/LOGIN",
-                  style: AppTextStyles.buttonText(
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            SizedBox(
+              height: screenWidth * 0.2,
+            ),
+            Container(
+              width: screenWidth * 0.7,
+              child: Image.asset("assets/images/StreamlineLogo.png"),
+            ),
+            SizedBox(
+              height: spacing / 2,
+            ),
+            Container(
+              width: screenWidth * 0.75,
+              child: Text(
+                "Please sign in to start enjoying special treatment from your favorite restaurant",
+                textAlign: TextAlign.center,
+                style: AppTextStyles.subtitleParagraph(
+                    screenHeight, AppColors.whiteText),
+              ),
+            ),
+            SizedBox(
+              height: spacing,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: spacing, right: spacing),
+              child: InputTextBox(
+                label: "Email",
+                hintText: "johndoe@gmail.com",
+                screenWidth: screenWidth,
+                screenHeight: screenHeight,
+              ),
+            ),
+            SizedBox(
+              height: spacing,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: spacing, right: spacing),
+              child: InputTextBox(
+                label: "Password",
+                hintText: "********",
+                screenWidth: screenWidth,
+                screenHeight: screenHeight,
+              ),
+            ),
+            SizedBox(
+              height: spacing * 2,
+            ),
+            ElevatedButton(
+                onPressed: signIn(),
+                child: Container(
+                  height: screenWidth * 0.12,
+                  width: screenWidth - spacing * 2,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColors.primaryColor),
+                  child: Center(
+                    child: Text(
+                      "SIGN UP/LOGIN",
+                      style: AppTextStyles.buttonText(
+                          screenHeight, AppColors.whiteText),
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: spacing,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 1,
+                  width: screenWidth * 0.3,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        AppColors.backgroundColor,
+                        Color(0xffD9D9D9)
+                      ], // Gradient colors
+                      begin: Alignment.topLeft, // Gradient start position
+                      end: Alignment.bottomRight, // Gradient end position
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: spacing / 2,
+                ),
+                Text(
+                  "Or continue with",
+                  style: AppTextStyles.subtitleParagraph(
                       screenHeight, AppColors.whiteText),
                 ),
-              ),
-            )),
-        SizedBox(
-          height: spacing,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 1,
-              width: screenWidth * 0.3,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    AppColors.backgroundColor,
-                    Color(0xffD9D9D9)
-                  ], // Gradient colors
-                  begin: Alignment.topLeft, // Gradient start position
-                  end: Alignment.bottomRight, // Gradient end position
+                SizedBox(
+                  width: spacing / 2,
                 ),
-              ),
-            ),
-            SizedBox(
-              width: spacing / 2,
-            ),
-            Text(
-              "Or continue with",
-              style: AppTextStyles.subtitleParagraph(
-                  screenHeight, AppColors.whiteText),
-            ),
-            SizedBox(
-              width: spacing / 2,
-            ),
-            Container(
-              height: 1,
-              width: screenWidth * 0.3,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    AppColors.backgroundColor,
-                    Color(0xffD9D9D9)
-                  ], // Gradient colors
-                  begin: Alignment.bottomRight, // Gradient start position
-                  end: Alignment.bottomLeft, // Gradient end position
+                Container(
+                  height: 1,
+                  width: screenWidth * 0.3,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        AppColors.backgroundColor,
+                        Color(0xffD9D9D9)
+                      ], // Gradient colors
+                      begin: Alignment.bottomRight, // Gradient start position
+                      end: Alignment.bottomLeft, // Gradient end position
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: spacing*2,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: screenWidth * 0.15,
-              width: screenWidth * 0.15,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: AppColors.whiteText, // Border color
-                    width: 2, // Border width
-                  ),
-                  borderRadius: BorderRadius.circular(10)),
+              ],
             ),
             SizedBox(
-              width: spacing * 2,
+              height: spacing * 2,
             ),
-            Container(
-              height: screenWidth * 0.15,
-              width: screenWidth * 0.15,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: AppColors.whiteText, // Border color
-                    width: 2, // Border width
-                  ),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ],
-        )
-      ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: screenWidth * 0.15,
+                  width: screenWidth * 0.15,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: AppColors.whiteText, // Border color
+                        width: 2, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                SizedBox(
+                  width: spacing * 2,
+                ),
+                Container(
+                  height: screenWidth * 0.15,
+                  width: screenWidth * 0.15,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: AppColors.whiteText, // Border color
+                        width: 2, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ],
+            )
+          ]),
+        ),
+      ),
     ));
   }
 
   signIn() {}
+
+  closeKeyboard(){}
 }
 
 class InputTextBox extends StatelessWidget {
