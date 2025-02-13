13bd4b0c7e46e78b4721e3f9f55e76230c19f9d3
ColeArduser <carduser@uiowa.edu>
2025-02-13 10:02:33 -0600

    SCRUM-12 basic django app

---
+++ backend/django_backend/api/urls.py
@@ -0,0 +1,8 @@
+# api/urls.py
+
+from django.urls import path
+from .views import login  # Import the login view
+
+urlpatterns = [
+    path('login/', login, name='login'),  # Map the /login endpoint to the login view
+]
