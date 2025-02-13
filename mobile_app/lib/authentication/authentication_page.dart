50bb8272e8f85a649d39fdf63d980263ec083de7
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2025-02-13 10:08:04 -0600

    added image as logo and working on the login

--- mobile_app/lib/authentication/authentication_page.dart
+++ mobile_app/lib/authentication/authentication_page.dart
@@ -18,62 +18,93 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
     double spacing = screenWidth * 0.05;
 
     return Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        appBar: AppBar(
-          leading: Text('app logo placeholder'),
-        ),
         body: Container(
-          width: screenWidth,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            SizedBox(
-              height: screenWidth * 0.2,
-            ),
-            Text(
-              "Hello!",
-              style: AppTextStyles.bigBoldLetters(
-                  screenHeight, AppColors.whiteText),
-            ),
-            SizedBox(
-              height: spacing,
-            ),
-            Container(
-              width: screenWidth * 0.75,
-              child: Text(
-                "Please sign in to start enjoying special treatment from your favorite restaurant",
-                textAlign: TextAlign.center,
-                style: AppTextStyles.subtitleParagraph(
-                    screenHeight, AppColors.whiteText),
+      width: screenWidth,
+      height: screenHeight,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff1A1D1A), Color(0xff313538)], // Gradient colors
+          begin: Alignment.topLeft, // Gradient start position
+          end: Alignment.bottomRight, // Gradient end position
+        ),
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        SizedBox(
+          height: screenWidth * 0.2,
+        ),
+        Container(
+          width: screenWidth * 0.7,
+          child: Image.asset("assets/images/StreamlineLogo.png"),
+        ),
+        SizedBox(
+          height: spacing,
+        ),
+        Container(
+          width: screenWidth * 0.75,
+          child: Text(
+            "Please sign in to start enjoying special treatment from your favorite restaurant",
+            textAlign: TextAlign.center,
+            style: AppTextStyles.subtitleParagraph(
+                screenHeight, AppColors.whiteText),
+          ),
+        ),
+        SizedBox(
+          height: spacing * 2,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: spacing, right: spacing),
+          child: InputTextBox(
+            label: "Email",
+            hintText: "johndoe@gmail.com",
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+          ),
+        ),
+        SizedBox(
+          height: spacing,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: spacing, right: spacing),
+          child: InputTextBox(
+            label: "Password",
+            hintText: "********",
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+          ),
+        ),
+        SizedBox(
+          height: spacing * 2,
+        ),
+        ElevatedButton(
+            onPressed: signIn(),
+            child: Container(
+              height: screenWidth * 0.12,
+              width: screenWidth - spacing * 2,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppColors.primaryColor),
+              child: Center(
+                child: Text(
+                  "SIGN UP/LOGIN",
+                  style: AppTextStyles.buttonText(
+                      screenHeight, AppColors.whiteText),
+                ),
               ),
-            ),
-            SizedBox(
-              height: spacing*2,
-            ),
-            
-            Padding(
-              padding: EdgeInsets.only(left: spacing, right: spacing),
-              child: InputTextBox(label: "Email", hintText: "johndoe@gmail.com",screenWidth: screenWidth, screenHeight: screenHeight,),
-            ),
-            SizedBox(
-              height: spacing,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: spacing, right: spacing),
-              child: InputTextBox(label: "Password", hintText: "********",screenWidth: screenWidth, screenHeight: screenHeight,),
-            ),
-          ]),
-        ));
+            ))
+      ]),
+    ));
   }
+
+  signIn() {}
 }
 
 class InputTextBox extends StatelessWidget {
-  const InputTextBox({
-    super.key,
-    required this.screenWidth,
-    required this.screenHeight,
-    required this.label,
-    required this.hintText
-  });
+  const InputTextBox(
+      {super.key,
+      required this.screenWidth,
+      required this.screenHeight,
+      required this.label,
+      required this.hintText});
 
   final double screenWidth;
   final double screenHeight;
@@ -85,18 +116,20 @@ class InputTextBox extends StatelessWidget {
     return Column(
       children: [
         Row(
-              children: [
-                Text(label,style: AppTextStyles.textFieldLabel(screenHeight, AppColors.whiteText),),
-              ],
+          children: [
+            Text(
+              label,
+              style: AppTextStyles.textFieldLabel(
+                  screenHeight, AppColors.whiteText),
             ),
-            
+          ],
+        ),
         Container(
-          height: screenWidth*0.12,
+          height: screenWidth * 0.12,
           child: TextField(
             decoration: InputDecoration(
                 filled: true,
                 fillColor: AppColors.whiteText,
-                
                 border: OutlineInputBorder(),
                 hintText: hintText),
           ),
