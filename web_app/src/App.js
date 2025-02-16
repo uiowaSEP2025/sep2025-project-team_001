e13c7271070985184dfb680956b4cbbd2d56b61c
Matt Stensby <mattystenz@gmail.com>
2025-02-16 15:58:12 -0600

    SCRUM-37 Work In Progress: Adjusted App.js and index.css to begin the setup of the Online Registration/signup page for the Web App. The goal within this task is to allow new business accounts to be created via our Web App.

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+function Signup() {
+  const navigate = useNavigate();
+  const handleRegister = (event) => {
+    console.log('Form submitted');
+    navigate('/login'); // Redirect to the home page after submission
+  };
+  return (
+    <Container className = "page-container">
+      <h1>Online Registration</h1>
+      <Form onSubmit = {handleRegister} className = "form-container">
+        <Form.Group controlId = "desiredUsername" className = "form-group-spacing">
+          <Form.Label>Desired Username</Form.Label>
+          <Form.Control type = "text" placeholder = "Enter desired username" required />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path ="/signup" element={<Signup />} />
+      {/* <Route path ="/login" element={<Login />} /> */}
+    </Routes>
+  )
+}
 
 function App() {
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <Button variant="primary">Login</Button>
-    </Container>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
 
