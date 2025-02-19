397a0f29e85a681e4d389409b8fc1903d78128da
Shaun Richter <shaunrichter547@gmail.com>
2025-02-18 18:22:13 -0600

    Adding routes to login and registration page

--- web_app/src/pages/Home.js
+++ web_app/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <Container className="text-center mt-5">
-      <Button variant="primary" className="m-2">Register</Button>
-      <Button variant="secondary" className="m-2">Log In</Button>
+      <Link to="/register">
+        <Button variant="primary" className="m-2">Register</Button>
+      </Link>
+      <Link to="/login">
+        <Button variant="secondary" className="m-2">Log In</Button>
+    </Link>
     </Container>
   );
 }
