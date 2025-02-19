7469128c0e6d19552028eb8a8ef87bf7f8eb4324
Shaun Richter <shaunrichter547@gmail.com>
2025-02-18 18:22:13 -0600

    Adding routes to login and registration page

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
