ddeade7e9d9036c7c6f845b1d5a0dc25aa8b86cf
ColeArduser <carduser@uiowa.edu>
2025-02-20 11:36:38 -0600

    SAVEPOINT: back and frotnend local and RDS working

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "public_subnet_a_id" {
+  description = "ID of the newly created public subnet a"
+  value       = aws_subnet.public_subnet_a.id
+}
+
+output "public_subnet_b_id" {
+  description = "ID of the newly created public subnet b"
+  value       = aws_subnet.public_subnet_b.id
+}
+
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance from the module"
+  value       = module.rds.db_endpoint
+}
