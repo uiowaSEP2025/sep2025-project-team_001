2fba9daf0b5966c8535ca41e065377aad1129311
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2025-02-26 15:48:40 -0600

    added home screen with app bar

--- mobile_app/lib/pages/home_page.dart
+++ mobile_app/lib/pages/home_page.dart
@@ -10,6 +10,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("successfully loged in", style: TextStyle(fontSize: 100),));
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    double horizontalSpacing = screenWidth * 0.05;
+    double verticalSpacing = screenHeight * 0.025;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Select a bar',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              print("Profile clicked");
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: horizontalSpacing, ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Select your Bar"),
+            ],
+          )],
+        ),
+      ),
+    );
   }
 }
