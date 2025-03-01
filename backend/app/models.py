8fb5d4f0e1c808fc2cde67a10c2a5169f67b2813
mstensby <stensbymatt@gmail.com>
2025-03-01 14:20:12 -0500

    Work in Progress: Adjusted the models.py to allow for a customer to have a user ID, created at, and updated at fields to be able to track customer accounts.

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,3 +6,12 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
     business_name = models.CharField(max_length=255, blank=True, null=True)
     business_address = models.TextField(blank=True, null=True)
+
+class Customer(Models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="customer")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Allows for the object to be displayed in the Django Admin
+    def __str__(self):
+        return self.user.username
