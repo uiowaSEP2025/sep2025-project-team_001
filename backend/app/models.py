eeb0cd0633f48fe71a77df1434ebe0fb0aa2a8b6
Shaun Richter <shaunrichter547@gmail.com>
2025-03-03 21:01:13 -0600

    Updated django for token authentication for users

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
     business_name = models.CharField(max_length=255, blank=True, null=True)
