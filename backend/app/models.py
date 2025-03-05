ec0472b81558bfa7473a011f9acbe083dc53982f
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2025-03-04 23:12:23 -0600

    Matched backend to the main backend

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,22 +6,3 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
     business_name = models.CharField(max_length=255, blank=True, null=True)
     business_address = models.TextField(blank=True, null=True)
-
-class Customer(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="customer")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "username": self.user.username,
-            "email": self.user.email,
-            "phone": self.user.phone,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
-        }
-
-    # Allows for the object to be displayed in the Django Admin
-    def __str__(self):
-        return f"{self.user.username}'s Customer Profile"
