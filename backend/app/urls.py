4f11ba148665a45509bc35a898296bbcb6d271c9
mstensby <stensbymatt@gmail.com>
2025-03-26 15:04:50 -0500

    BUG: Had to adjust style of the menu view so that it mimicked what we currently had for the auth views. This was a mistake on my end, as I did not understand that they all should be using the same methodology to test our API endpoints.

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from app.views.auth_views import login_user, register_user
 from app.views.orders_views import CurrentItemsList
-from app.views.menu_views import menu_items_api, MenuPageView
+from app.views.menu_views import menu_items_api, manage_menu_item
 
 urlpatterns = [
     path('login/', login_user, name='login'),
@@ -11,5 +11,5 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('bartender_view/', CurrentItemsList.as_view(), name='bartender_view'),
     path('api/menu-items/', menu_items_api, name='menu_items_api'),
-    path('menu/', MenuPageView.as_view(), name='menu_page'),
+    path('api/manage-item/', manage_menu_item, name='manage_menu_item'),
 ]
