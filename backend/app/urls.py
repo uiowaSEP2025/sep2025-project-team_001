a7ec3f0613b6d79466870c538ae1f15a3d3f822e
mstensby <stensbymatt@gmail.com>
2025-03-26 13:48:18 -0500

    Adjusted the URLs to be able to represent the API keys hitting properly through api/menu-items as well as creating a menu page path where the manager will be able to adjust their menu.

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,11 +1,15 @@
+# urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from app.views.auth_views import login_user, register_user
 from app.views.orders_views import CurrentItemsList
+from app.views.menu_views import menu_items_api, MenuPageView
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register_user, name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('bartender_view/', CurrentItemsList.as_view(), name='bartender_view'),
+     path('api/menu-items/', menu_items_api, name='menu_items_api'),
+     path('menu/', MenuPageView.as_view(), name='menu-page'),
 ]
