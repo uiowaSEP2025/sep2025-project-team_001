768136d2897d699903cbb262e9cd0dc5114d264d
mstensby <stensbymatt@gmail.com>
2025-03-26 15:59:42 -0500

    Committing files to switch over to Rafa's Branch

---
+++ web_app/src/pages/MenuPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getMenuItems } from '../api/menuApi';
+import '../styles/MenuPage.css';
+
+const MenuPage = () => {
+
+}
