1c9dd2daa119cbd435bf57bb83ea61faa10bf38b
mstensby <stensbymatt@gmail.com>
2025-03-26 13:43:32 -0500

    SCRUM-21 Added post functionality for the manager to be able to add the menu items. This allows for removing unecessary additional functionality that was previously apart of two seperate classes. There is now one, confluent class that takes care of the desired API requests.

--- backend/app/views/menu_views.py
+++ backend/app/views/menu_views.py
@@ -1,7 +1,10 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from app.models.restaurant_models import Restaurant, Item
-class MenuListCreateView(View):
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+
+class MenuListCreateView(View, LoginRequiredMixin):
     template_name = "menu_list.html"
 
     def get(self, request, *args, **kwargs):
@@ -15,40 +18,50 @@ class MenuListCreateView(View):
             'items': items
             })
     def post(self, request, *args, **kwargs):
-
+        action = request.POST.get('action', '')
+        item_id = request.POST.get('item_id', None)
         restaurant = Restaurant.objects.first()
         if not restaurant:
             return redirect('menu_list')
         
-        
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        price = request.POST.get('price')
-        category = request.POST.get('category')
-        stock = request.POST.get('stock')
-        available = bool(request.POST.get(available, True))
+        if action == 'create':
+            name = request.POST.get('name')
+            description = request.POST.get('description', '')
+            price = float(request.POST.get('price', 0.0))
+            category = request.POST.get('category', '')
+            stock = int(request.POST.get('stock', 0))
+            available = request.POST.get('available', 'false') == 'true'
 
-        Item.objects.create(
-            restaurant=restaurant,
-            name=name,
-            description=description,
-            price=price,
-            category=category,
-            stock=stock,
-            available=available
-        )
+            Item.objects.create(
+                restaurant=restaurant,
+                name=name,
+                description=description,
+                price=price,
+                category=category,
+                stock=stock,
+                available=available
+            )
+        elif action == 'update' and item_id:
+            item = get_object_or_404(Item, pk=item_id, restaurant=restaurant)
+            item.name = request.POST.get('name', item.name)
+            item.description = request.POST.get('description', item.description)
+            item.price = float(request.POST.get('price', item.price))
+            item.category = request.POST.get('category', item.category)
+            item.stock = int(request.POST.get('stock', item.stock))
+            item.available = request.POST.get('available', 'false') == 'true'
+            item.save()
+        elif action == 'delete' and item_id:
+            item = get_object_or_404(Item, pk=item_id, restaurant=restaurant)
+            item.delete()
 
         return redirect('menu_list')
-    
-class MenuDetailView(View):
-    template_name = 'menu_detail.html'
-
-    def get(self, request, pk, *args, **kwargs):
-        # Logic to retrieve and display the details of a specific menu item
-        restaurant = Restaurant.objects.first()
-        item = get_object_or_404(Item, pk=pk, restaurant=restaurant)
-
-    
+    def menu_items_api(request):
+        if request.method == 'GET':
+            restaurant = Restaurant.objects.first()
+            if not restaurant:
+                return JsonResponse({'error': 'Restaurant not found'}, status=404)
+            items = list(restaurant.items.values())
+            return JsonResponse({'items': items}, status=200)
 
         
 
