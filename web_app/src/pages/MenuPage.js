934020920eb62a7a5bd74c58d0f4d183a3ce1cb6
mstensby <stensbymatt@gmail.com>
2025-03-27 12:57:23 -0500

    SCRUM-21 Creating a simple button that allows the manager to create menu items

--- web_app/src/pages/MenuPage.js
+++ web_app/src/pages/MenuPage.js
@@ -4,5 +4,30 @@ import { getMenuItems } from '../api/menuApi';
 import '../styles/MenuPage.css';
 
 const MenuPage = () => {
+    // Modals are used for being able to create 'popup' forms for creating and deleting items
+    const [items, setItems] = useState([]);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [deleteItemId, setDeleteItemId] = useState(null);
 
+    const openCreateModal = () => {
+        setFormData({
+            name: '',
+            description: '',
+            price: '',
+            category: '',
+            stock: '',
+            available: true
+        });
+        setShowCreateModal(true);
+    };
+    return (
+        <div className = 'menu-page-container'>
+            <h2>Menu Manager</h2>
+
+            <button className = "create-button" onClick={openCreateModal}>
+                Create New Item
+            </button>
+        </div>
+    )
 }
