a21e82fb270f7571cc4c7d2c2ea5db632c623635
mstensby <stensbymatt@gmail.com>
2025-03-27 13:21:13 -0500

    SCRUM-21 Starting to create the headers for being able to differentiate between the Available and Unavailable items. Then, I began working on being able to toggle an item's availablitiy.

--- web_app/src/pages/MenuPage.js
+++ web_app/src/pages/MenuPage.js
@@ -10,6 +10,22 @@ const MenuPage = () => {
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [deleteItemId, setDeleteItemId] = useState(null);
 
+    const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        price: '',
+        category: '',
+        stock: '',
+        available: true
+    });
+    
+    const fetchItems = () => {
+        getMenuItems()
+            .then(data => setItems(data))
+            .catch(error => console.error('Error fetching menu items:', error));
+    
+        };
+
     const openCreateModal = () => {
         setFormData({
             name: '',
@@ -21,13 +37,61 @@ const MenuPage = () => {
         });
         setShowCreateModal(true);
     };
-    return (
-        <div className = 'menu-page-container'>
-            <h2>Menu Manager</h2>
 
-            <button className = "create-button" onClick={openCreateModal}>
-                Create New Item
-            </button>
+    const toggleAvailability = (item) => {
+        const newAvailability = !item.available;
+        const data = new FormData();
+        data.append('action', 'update')
+        data.append('item_id', item.id);
+        data.append('name', item.name);
+        data.append('description', item.description);
+        data.append('price', item.price);
+        data.append('category', item.category);
+        data.append('stock', item.stock);
+        data.append('available', newAvailability? 'true' : 'false');
+
+        fetch('/api/menu', {
+            method: 'POST',
+            body: data
+        })
+        .then(() => {
+            fetchItems();
+        })
+        .catch(error => {
+            console.error('Error updating item availability:', error);
+        });
+    }
+    return (
+        <div>
+            <div className = "menu-page-container">
+                <h2>Menu Manager</h2>
+
+                <button className = "create-button" onClick={openCreateModal}>
+                    Create New Item
+                </button>
+            </div>
+
+            <div className = "section-header">Available Items</div>
+                <div className = "sub-section-header">Beverages</div>
+                {availableBeverages.length == 0 && <p>No available beverages</p>}
+                {availableBeverages.map(item => (
+                    <div key={item.id} className="item-container">
+                        <div className="item-card">
+                            <h3>{item.name}</h3>
+                            {item.description}<br />
+                            Price: ${item.price}<br />
+                            Stock: {item.stock}<br />
+                            Available: {' '}
+                        </div>
+                        <input
+                            type="checkbox"
+                            checked={item.available}
+                            onChange={() => toggleAvailability(item)}
+                        />
+                    </div>
+                ))}
+            <div className = "section-header">Unavailable Items</div>
         </div>
     )
+    
 }
