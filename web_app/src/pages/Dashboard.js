c990d3e64d4bec08b7586ecf367c0d7317ef7e9e
mstensby <stensbymatt@gmail.com>
2025-04-08 12:40:52 -0500

    SCRUM-41 Added a modal so that the owner must be authenticated before routing to the manager creation page.

--- web_app/src/pages/Dashboard.js
+++ web_app/src/pages/Dashboard.js
@@ -1,13 +1,20 @@
 // src/pages/Dashboard.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import OwnerAuthModal from '../components/OwnerAuthModal';
 
 function Dashboard() {
   const navigate = useNavigate();
-
+  const [ownerInfo, setOwnerInfo] = useState(null);
+  const [showAuthModal, setShowAuthModal] = useState(false);
   const barName = sessionStorage.getItem('barName');
+
+  const handleAuthenticated = (data) => {
+    setOwnerInfo(data);
+    navigate('manager_registration');
+  }
 
   const handleOrderClick = () => {
     navigate('/orders');
@@ -37,6 +44,20 @@ function Dashboard() {
       <Button variant="danger" size="lg" onClick={handleLogOutClick}>
         Log Out
       </Button>
+      <Button
+        variant="warning"
+        size="lg"
+        className="mt-3"
+        onClick={() => setShowAuthModal(true)}
+      >
+        Create Manager
+      </Button>
+      <OwnerAuthModal
+        show={showAuthModal}
+        onHide={() => setShowAuthModal(false)}
+        onOwnerAuthenticated={handleAuthenticated}
+      />
+ 
     </Container>
   );
 }
