285bea248316c2cbb3285042aa7b03e1564f9ddf
ColeArduser <carduser@uiowa.edu>
2025-04-10 13:23:06 -0500

    added scheduler instance to create scheduler on creation of the Django app

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from app.utils.multi_bartender_scheduler import MultiBartenderScheduler
 
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
+
+    def ready(self):
+        from app import scheduler_instance
+        # Adjust the number of bartenders as needed—here we use 2.
+        scheduler_instance.multi_bartender_scheduler = MultiBartenderScheduler(num_bartenders=2)
