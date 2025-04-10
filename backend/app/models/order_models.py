b2c012bf783ee1afb7cc481d04f5273311e31b2f
ColeArduser <carduser@uiowa.edu>
2025-04-10 13:19:17 -0500

    added eta to Order model and performed migration

--- backend/app/models/order_models.py
+++ backend/app/models/order_models.py
@@ -14,6 +14,7 @@ class Order(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default="pending")
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    estimated_pickup_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.user.username} at {self.restaurant.name}"
