0018f01e9f11a92d37377970bad596891142341b
Shaun Richter <shaunrichter547@gmail.com>
2025-04-16 14:58:35 -0500

    Bug fix

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,7 +2,7 @@ from .customer_models import Customer, CustomUser
 from .order_models import Order, OrderItem
 from .restaurant_models import Ingredient, Item, Restaurant
 from .worker_models import Worker
-from review_models import Review
+from .review_models import Review
 
 __all__ = [
     "CustomUser",
