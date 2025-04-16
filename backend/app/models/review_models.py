bdb3382daa9ec37a7d39c2fdb8fc0ddfab973132
Shaun Richter <shaunrichter547@gmail.com>
2025-04-16 13:26:51 -0500

    Removed worker and items from review model as those are retreived through order instance

--- backend/app/models/review_models.py
+++ backend/app/models/review_models.py
@@ -4,12 +4,8 @@ from .restaurant_models import Item, Worker
 
 class Review(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='review')
-    worker = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviews')
-    items = models.ManyToManyField(Item, related_name='reviewed_in', blank=True)  # Items mentioned in the review
-
     rating = models.PositiveIntegerField(default=5)  # 1-5 scale
     comment = models.TextField(blank=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
