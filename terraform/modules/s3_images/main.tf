d6247029f87fdba02707f7c739ad916f2c3507bb
ColeArduser <carduser@uiowa.edu>
2025-04-30 20:46:06 -0500

    added s3 policy

--- terraform/modules/s3_images/main.tf
+++ terraform/modules/s3_images/main.tf
@@ -13,3 +13,30 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.images.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.images.bucket}/*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+}
